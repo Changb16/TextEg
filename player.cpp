98f86880abe604c11c9ff272bb7890f4e23aabdc
RhinoIzumi <43182794+RhinoIzumi@users.noreply.github.com>
2018-11-13 16:40:26 -0500

    Room parser: almost done
    I think the problem is in lines 96-104.

--- player.cpp
+++ player.cpp
@@ -4,7 +4,6 @@
 #include <cstdlib>
 #include <stdlib.h>
 #include <string>
-#include <fstream>
 #include <sstream>
 #include <cstdio>
 #include "tinyxml2.h"
@@ -17,15 +16,15 @@ struct data
     string title;
     string author;
 	int roomNum;
-	string dialog;
+	int testNum;
+	string description;
 };
 
 void player(string game)
 {
     string name = (game + ".xml");
-    string title, author;
-    int choice, i = 0, j = 1;
-    ostringstream in_str;
+    string title, author, description;
+    int choice = 1, i = 0;
     stringstream ss;
 
     cin.clear();
@@ -35,40 +34,78 @@ void player(string game)
     XMLDocument project;
     project.LoadFile(name.c_str());
 
-    if(project.ErrorID() == 0)
+    XMLElement *pRoot;
+    pRoot = project.FirstChildElement("text_adventure");
+    XMLElement *pRoom;
+    pRoom = pRoot->FirstChildElement("room");
+
+    data roomA;
+    data * proom;
+    proom = &roomA;
+
+    proom->title = pRoot->FirstChildElement("game")->GetText();
+    proom->author = pRoot->FirstChildElement("author")->GetText();
+
+    cout<<"'" + roomA.title + "'" + " by " + roomA.author<<endl<<endl;
+
+    while(choice != 0)
     {
-        XMLElement *pRoot;
-        pRoot = project.FirstChildElement("text_adventure");
+        proom->title = pRoom->FirstChildElement("title")->GetText();
+        cout<<roomA.title<<endl;
+        proom->description = pRoom->FirstChildElement("description")->GetText();
+        cout<<roomA.description<<endl<<endl;
 
-        data roomA;
-        data * proom;
-        proom = &roomA;
+        ss.str("");
+        i = 1;
+        ss<<i;
+        proom->roomNum = 0;
+        proom->testNum = 0;
+        pRoom = pRoom->FirstChildElement(("description_" + ss.str()).c_str());
 
-        proom->title = pRoot->FirstChildElement("game")->GetText();
-        proom->author = pRoot->FirstChildElement("author")->GetText();
-
-        cout<<roomA.title + " by " + roomA.author<<endl<<endl;
-
-        proom->dialog = pRoot->FirstChildElement("room")->FirstChildElement("description")->GetText();
-        cout<<roomA.dialog<<endl;
-/*
-        in_str<<j;
-        string description = "description_" + in_str.str();
-
-        while(pRoot->FirstChildElement("room")->FirstChildElement(description.c_str()))
+        while(pRoom)
         {
+            ss.str("");
+            ss<<i;
 
-            proom->dialog = pRoot->FirstChildElement("room")->FirstChildElement(description.c_str())->GetText();
-            ss<< j << ".) " << roomA.dialog;
-            cout<<ss.str()<<endl;
+            proom->description = pRoom->GetText();
+            cout<<ss.str() + ".) " + roomA.description<<endl;
 
-            j++;
-            //in_str<<j;
-            string description = "description_" + in_str.str();
+            i++;
+            ss.str("");
+            ss<<i;
+            pRoom = pRoom->NextSiblingElement(("description_" + ss.str()).c_str());
         }
-*/
-        cout<<"What action would you like to take?: ";
+
+        cout<<"0.) Quit playing."<<endl;
+        cout<<endl<<"What action would you like to take?: ";
         cin >> choice;
+        cout<<endl;
+
+        if(choice > 0)
+        {
+            ss.str("");
+            ss<<choice;
+
+            while(roomA.roomNum == 0)
+            {
+                pRoom = pRoot->FirstChildElement("room");
+
+                proom->roomNum = atoi(pRoom->FirstChildElement(("connection_" + ss.str()).c_str())->GetText());
+            }
+
+            while(roomA.roomNum != roomA.testNum)
+            {
+                proom->testNum = atoi(pRoom->FirstChildElement("room_num")->GetText());
+                pRoom = pRoom->NextSiblingElement();
+
+                //cout<<roomA.testNum<<endl;
+            }
+
+            pRoom = pRoom->PreviousSiblingElement();
+        }
+
+        else
+            break;
     }
 
     return;
