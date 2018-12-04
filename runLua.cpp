9199aa5f2a6fcae6270b9b6c4d3d3a6cc8a77897
Changb16 <40473710+Changb16@users.noreply.github.com>
2018-12-04 15:54:57 -0500

    Add files via upload

--- runLua.cpp
+++ runLua.cpp
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <iostream>
-#include <string>
 
 //Include lua headers
 #include "lua.h"
@@ -12,29 +11,29 @@
 
 using namespace std;
 
-void runLua(string script){
+int main(int argc, char ** argv){
 	int cursed=0;
-/*
+
     //iterate all files and execute
     for(int n=1; n<argc; n++){
         char * file = argv[n];
- */
+ 
         //create a new lua state
         lua_State * L = luaL_newstate();
  
         //open all libraries
         luaL_openlibs(L);
 	
-	//cout<<"Script start"<<endl;
+	cout<<"Script start"<<endl;
  	
-        int s = luaL_loadfile(L, script);
+        int s = luaL_loadfile(L, file);
 
 	s = lua_pcall(L, 0, LUA_MULTRET, 0);
  
         lua_close(L);
 
-	//cout<<"Script end"<<endl;
+	cout<<"Scipt end"<<endl;
     }
  
-    return;
+    return 0;
 }
